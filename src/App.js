76adf9139f07169eec800f86c6581a74de7f23c3
hunter <hantek.f@gmail.com>
2020-03-21 03:33:10 +0100

    move analytics to index

--- src/App.js
+++ src/App.js
@@ -3,21 +3,18 @@ import Navigation from './Navigation/Navigation'
 import Header from './Header/Header'
 //import { Helmet } from 'react-helmet';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="Site">
-        <Header />
-        <main className="Site-content">
-          <Navigation />
-        </main>
-        <nav className="navbar absolute navbar-light bg-light">
-          <p id="middle"> © Hunter 2020</p>
-        </nav>
-      </div>
-    </BrowserRouter>
+    <div className="Site">
+      <Header />
+      <main className="Site-content">
+        <Navigation />
+      </main>
+      <nav className="navbar absolute navbar-light bg-light">
+        <p id="middle"> © Hunter 2020</p>
+      </nav>
+    </div>
   );
 };
 export default App;
