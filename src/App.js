32446dd993de86b6a5a867a6083efbea41ad90b3
hunter <hantek.f@gmail.com>
2020-03-21 03:05:56 +0100

    try GA again

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import Navigation from './Navigation/Navigation'
 import Header from './Header/Header'
 //import { Helmet } from 'react-helmet';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import GA from './utils/GoogleAnalytics'
 
 function App() {
   return (
-    <div className="Site">
-      <Header />
-      <main className="Site-content">
-        <Navigation />
-      </main>
-      <nav className="navbar absolute navbar-light bg-light">
-        <p id="middle"> © Hunter 2020</p>
-      </nav>
-    </div>
+    <BrowserRouter>
+      {GA.init() && <GA.RouteTracker />}
+      <div className="Site">
+        <Header />
+        <main className="Site-content">
+          <Navigation />
+        </main>
+        <nav className="navbar absolute navbar-light bg-light">
+          <p id="middle"> © Hunter 2020</p>
+        </nav>
+      </div>
+    </BrowserRouter>
   );
 };
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,16 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
-import GAListener from './GAListener';
 
-ReactDOM.render(
-    <BrowserRouter>
-        <GAListener trackingId="UA-161475072-1">
-            <App />
-        </GAListener>
-    </BrowserRouter>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
