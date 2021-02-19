6b00beca481cad17af3de93b7a55984b64709df7
hunter <hantek.f@gmail.com>
2021-02-19 18:08:42 +0100

    updated package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Navigation from './Navigation/Navigation'
 import Header from './Header/Header'
-//import { Helmet } from 'react-helmet';
 import './App.css';
 
 function App() {
